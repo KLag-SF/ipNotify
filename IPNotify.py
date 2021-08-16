4cb544d4e37b41af49d8bf42040fbc57d919fbf9
KLag-SF <53226234+KLag-SF@users.noreply.github.com>
2021-08-17 03:11:18 +0900

    Update IPNotify.py

--- IPNotify.py
+++ IPNotify.py
@@ -56,7 +56,7 @@ def main_routine():
                 notify(current_IP)
 
         finally:
-            time.sleep(10)
+            time.sleep(0)
 
 # Run this program as linux daemon
 def daemonize():
